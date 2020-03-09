54486c53a243baa33e55878037f305e16539d77a
unknown <jake_stetter@hotmail.com>
2020-03-09 16:19:14 -0700

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,26 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select('tbody');
+
+tableData.forEach(function(ufoSiting) {
+   console.log(ufoSiting);
+   var row = tbody.append("tr");
+   Object.entries(ufoSiting).forEach(function([key, value]) {
+     console.log(key, value);
+     var cell = row.append("td");
+     cell.text(value);
+   });
+ });
+
+/*
+for (var i = 0; i < tableData.length; i++) {
+    console.log(tableData[i]);
+    var row = tbody.append("tr");
+    for (var j = 0; j < tableData[i].length; j++) {
+        console.log(tableData[i].datetime)
+    }
+  }
+*/
+
+
